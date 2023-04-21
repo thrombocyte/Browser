6bd6915d4fbd3baaf4fc82c16cfebf7b790ae11e
Reuben Kaufman <reubenkaufman@gmail.com>
2023-04-21 11:15:44 -0500

    Complete chapter three

--- browser.py
+++ browser.py
@@ -1,14 +1,24 @@
 import sys, os, ssl 
 import tkinter 
+import tkinter.font
+
+
+
 #import urllib.parse: helpful ibrary, unused herein 
 #url = "http://example.org/index.html"
 
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 SCROLL_STEP = 100
+FONTS = {}
 
 
-
+def get_font(size, weight, slant):
+    key = (size, weight, slant)
+    if key not in FONTS:
+        font = tkinter.font.Font(size = size, weight = weight, slant = slant)
+        FONTS[key] = font
+    return FONTS[key]
     
 def request(url):
     #old line: assert url.startswith("http://")
@@ -66,38 +76,96 @@ def request(url):
     return headers, body 
 
 def lex(body):
-    in_angle = False 
+    out = []
     text = ""
+    in_tag = False 
     for c in body:
         if c == "<":
-            in_angle = True 
+            in_tag = True
+            if text: out.append(Text(text))
+            text = "" 
         elif c == ">":
-            in_angle = False 
-        elif not in_angle:
+            in_angle = False
+            out.append(Tag(text))
+            text = "" 
+        else:
             text += c
-    return text 
+    if not in_tag and text:
+        out.append(Text(text))
+    return out  
    
-def layout(text):
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
-        if cursor_x >= WIDTH-HSTEP:
-            cursor_x = HSTEP
-            cursor_y += VSTEP
-    
-    return display_list 
+class Layout:
+    def __init__(self, tokens):
+        self.display_list = []
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 16
+        self.line = []
+
+        for tok in tokens:
+            self.token(tok)
+        self.flush()
+
+    def token(self, tok):
+        if isinstance(tok, Text):
+            self.text(tok)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
+
+    def text(self, tok):
+        font = get_font(self.size, self.weight, self.style)
+
+        for word in tok.text.split():
+            w = font.measure(word)
+            if self.cursor_x + w >= WIDTH-HSTEP:
+                self.flush()
+            self.line.append((self.cursor_x, word, font))
+            self.cursor_x += w + font.measure(" ") 
+
+    def flush(self):
+        if not self.line: return  
+        metrics = [font.metrics() for x, word, font, in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        self.cursor_x = HSTEP
+        self.line = []
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline  + 1.25 * max_descent 
 
 
 class Browser:
-
     def __init__(self):
         self.window = tkinter.Tk()
+        self.window.title('Reuben Browser')
         self.canvas = tkinter.Canvas(
             self.window, 
             width = WIDTH,
             height = HEIGHT
+            
         )
         self.canvas.pack()
 
@@ -106,22 +174,18 @@ class Browser:
         self.window.bind("<Button-4>", self.scrollup)
         self.window.bind("<Button-5>", self.scrolldown)
 
-
     def load(self, url):
         headers, body = request(url)
-        text = lex(body)
-        self.display_list = layout(text)
+        tokens = lex(body)
+        self.display_list = Layout(tokens).display_list
         self.draw()
-
 
     def draw(self):
         self.canvas.delete("all")
-        for x, y, c in self.display_list:
+        for x, y, c, f in self.display_list:
             if y > self.scroll + HEIGHT: continue 
             if y + VSTEP < self.scroll: continue 
-            self.canvas.create_text(x, y - self.scroll, text=c)
-
-
+            self.canvas.create_text(x, y - self.scroll, text=c, anchor = 'nw', font= f)
 
     def scrolldown(self, event):
         self.scroll += SCROLL_STEP
@@ -131,11 +195,13 @@ class Browser:
         self.scroll -= SCROLL_STEP
         self.draw()
 
+class Text:
+    def __init__(self, text):
+        self.text = text
 
-
-
-
-
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
 
 if __name__ == '__main__':
     Browser().load(sys.argv[1])
